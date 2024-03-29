5a412dfadb7798fdcce020cb8e724ae5bf9ef784
Sergey Zevalev <sergeizevalyov@yandex.ru>
2024-03-29 19:26:30 +0300

    011 - solved

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -2,10 +2,36 @@ package ru.easycode.zerotoheroandroidtdd
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var count: Count
+    private var step = 2
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        count = Count.Base(step)
+
+        val textView = findViewById<TextView>(R.id.countTextView)
+        val button = findViewById<Button>(R.id.incrementButton)
+
+        button.setOnClickListener {
+            val number = textView.text.toString()
+            textView.text = (count as Count.Base).increment(number)
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putInt("KEY", step)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        step = savedInstanceState.getInt("KEY")
+        count = Count.Base(step)
     }
 }
